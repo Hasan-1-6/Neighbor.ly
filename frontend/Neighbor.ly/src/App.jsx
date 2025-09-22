b5ec1096c8d92f9b25e3ba47245c29f519523432
Hasan-1-6 <hasandilshad16@gmail.com>
2025-09-23 00:58:18 +0530

    first commit

---
+++ frontend/Neighbor.ly/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    // check if tailwind is working correctly
+    // install appropriate extensions like ES7+ React, Prettier, TailwindCSS 
+
+    <div className='text-red-400'>App</div> 
+    
+  )
+}
+
+export default App
