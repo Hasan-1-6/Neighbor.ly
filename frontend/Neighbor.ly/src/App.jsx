50564c39a7da7552ae37f43207fc04af6746c931
Divyansh1Rai6 <divyansh16rai@gmail.com>
2025-10-01 15:24:33 +0530

    updated App.jsx with router changes

--- frontend/Neighbor.ly/src/App.jsx
+++ frontend/Neighbor.ly/src/App.jsx
@@ -1,44 +1,41 @@
 import { useState } from 'react'
-
-// import Dashboard from './sides/Header/Dashboard'
-// import Grievances from './sides/Header/Grievances'
-// import Members from './sides/Header/Members'
+ import Dashboard from './sides/Header/Dashboard'
+ import Grievances from './sides/Header/Grievances'
+ import Members from './sides/Header/Members'
 import Navbar from './user/Navbar'
 import Footer from './user/Footer'
-// import Profile from './sides/Header/Profile'
+ import Profile from './sides/Header/Profile'
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
 import Topbar from './user/Topbar'
 import { Notification, Setting, Account } from './user/Topnav'
 
 function App() {
-  // const router = createBrowserRouter([
-  //   {
-  //     path : '/',
-  //     element : (
-  //       <>
-  //       <Topbar/>
-  //       <Navbar/>
-  //       <Outlet/>
-  //       <Footer/>
-  //       </>
-  //     ),
-  //     children: [
-  //       {index:true, element: <Dashboard/>},
-  //       {path: '/grev', element: <Grievances/>},
-  //       {path: '/members', element: <Members/>},
-  //       {path: '/pro', element: <Profile/>},
-  //       {path: '/notification', element: <Notification/>},
-  //       {path: '/setting', element: <Setting/>},
-  //       {path: '/account', element: <Account/>},
-  //     ],
-  //   },
+   const router = createBrowserRouter([
+     {
+       path : '/',
+       element : (
+        <>
+        <Topbar/>
+         <Navbar/>
+         <Outlet/>
+       <Footer/>
+         </>
+      ),
+       children: [
+         {index:true, element: <Dashboard/>},
+         {path: '/grev', element: <Grievances/>},
+         {path: '/members', element: <Members/>},
+       {path: '/pro', element: <Profile/>},
+         {path: '/notification', element: <Notification/>},
+         {path: '/setting', element: <Setting/>},
+         {path: '/account', element: <Account/>},
+       ],
+    },
 
-  // ])
+   ])
   return (
-    // <RouterProvider router={router} />
-    <>
-    <Navbar/>
-    </>
+    <RouterProvider router={router} />
+
   )
 }
 
